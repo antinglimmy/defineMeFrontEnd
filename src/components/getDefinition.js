e8b7b3be3bcc5574580d7609e3b87d7d58af6b23
antinglimmy <antinglim@gmail.com>
2022-11-28 20:40:20 +0800

    28 Nov

--- src/components/getDefinition.js
+++ src/components/getDefinition.js
@@ -1,38 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./pages.css";
-// import DefinitionPopUp from "./definitionPopUp";
-let definitions;
-const definitionArray = [];
 
-const GetDefinition = ({ isClicked, selectedWord, selectedDivObject }) => {
+let definitions;
+let definitionArray = [];
+
+const GetDefinition = ({ isClicked, selectedWord, selectedDiv }) => {
   const [metaDefinition, setMetaDefinition] = useState([]);
-  const [definition, setDefinition] = useState();
-  const [selected, setSelected] = useState();
+  const [selected, setSelected] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   const dictionaryRequest = () => {
-    // isClicked = false;
-    // useEffect(() => {
     axios
       .get(
         `${process.env.REACT_APP_DICTIONARY_URL}/${selectedWord}?key=${process.env.REACT_APP_DICTIONARY_API_KEY}`
       )
       .then((res) => {
         setMetaDefinition(res.data);
-        console.log("MetaData", res.data);
-        // isClicked = false;
+        // console.log("MetaData", res.data);
       })
       .catch((err) => console.log(err));
   };
-  // }, []);
 
   const showDefinition = () => {
-    // console.log("MetaDefinition", metaDefinition);
     const metaDefinitionSliced = metaDefinition.slice(0, 2);
-    console.log(metaDefinitionSliced);
-    // console.log(metaDefinitionSliced[0]);
-    // console.log(metaDefinitionSliced[0].shortdef[0]);
-    // console.log(metaDefinitionSliced[1].shortdef[0]);
+    // console.log(metaDefinitionSliced);
 
     for (let i = 0; i < metaDefinitionSliced.length; i++) {
       const currentDefinition = metaDefinitionSliced[i].shortdef[0];
@@ -40,50 +32,61 @@ const GetDefinition = ({ isClicked, selectedWord, selectedDivObject }) => {
     }
 
     console.log(definitionArray);
-
-    // metaDefinitionSliced.map((entry) => {
-    //   const definitionsSeparate = entry.shortdef;
-    //   definitionArray.push(definitionsSeparate);
-    //   const definitionArray2 = definitionArray.flat(1);
-    //   console.log(definitionArray2);
-    //   // console.log("definitions", definitions);
-    //   // const definitionCompiled = definitionsSeparate.flatMap((num) => num);
-    //   // console.log("definitionCompiled", definitionCompiled);
-    // });
   };
-  // hawkerNames = hawkerDetails.map((hawkerName) => {
-  //   const hawkerNameList = hawkerName.Name;
-  //   return hawkerNameList;
-  // });
-  const displayDefinitions = definitionArray.map((entry) => {
-    // return <div key="entry">{entry} </div>;
-    return <div>{entry} </div>;
-  });
+
+  // const displayDefinitions = () => {
+  //   definitionArray.map((entry) => {
+  //     return <div>{entry} </div>;
+  //   });
+  //   //   setShowButton(false);
+  //   //   setSelected(false);
+  //   //   console.log(selected, "selected Line 44");
+  // };
 
   const getDefinition = () => {
+    setSelected(true);
     dictionaryRequest();
     showDefinition();
-    setSelected(true);
+    // displayDefinitions();
+
+    console.log(selected, "selected Line 51");
   };
 
+  // console.log(isClicked, showButton, selected);
+
   return (
-    // isClicked && (
     <>
-      {isClicked && <button onClick={getDefinition}>Define Me</button>}
-      <div>Definition here</div>
+      {isClicked && (
+        <button
+          onClick={getDefinition}
+          // className={showButton ? "DefineMe" : "NotDefineMe"}
+          style={{
+            left: selectedDiv.left - 0,
+            top: selectedDiv.top - 40,
+          }}
+        >
+          Define Me
+        </button>
+      )}
+
+      {/* <div>{displayDefinitions()}</div> */}
+      {/* <div>{selected} -- selected value here</div>
+      {selected && <div>{displayDefinitions()}</div>} */}
       {selected && (
         <div
           className="definitionPopUp"
           style={{
-            selectedDivObject: selectedDivObject.left + 50,
-            top: selectedDivObject.left + 50,
+            left: selectedDiv.left - 0,
+            top: selectedDiv.top - 40,
           }}
         >
-          {displayDefinitions}
+          {definitionArray.map((entry) => {
+            return <div>{entry} </div>;
+          })}
+          {console.log(selected, "selected Line 79")}
+          {console.log("Line 80 happens")}
         </div>
       )}
-      {/* {displayDefinitions} */}
-      {/* <DefinitionPopUp definitions={definitions} /> */}
     </>
     // )
   );
--- src/components/selectText.js
+++ src/components/selectText.js
@@ -1,50 +1,6 @@
-// function foo() {
-//   const selObj = window.getSelection();
-//   alert(selObj);
-//   const selRange = selObj.getRangeAt(0);
-//   // do stuff with the range
-// }
-
-// From <input id="from" disabled> – To <input id="to" disabled>
-// <script>
-//   document.onselectionchange = function() {
-//     let selection = document.getSelection();
-
-//     let {anchorNode, anchorOffset, focusNode, focusOffset} = selection;
-
-//     // anchorNode and focusNode are text nodes usually
-//     from.value = `${anchorNode?.data}, offset ${anchorOffset}`;
-//     to.value = `${focusNode?.data}, offset ${focusOffset}`;
-//   };
-
-// const GetSelectionHandler = () => {
-//   const selection = window.getSelection();
-//   console.log("Got selection", selection.toString());
-// };
-
-// export default GetSelectionHandler;
-
-// const selection = useRef("");
-
-// const GetSelectionHandler = () => {
-//   const text = document.getSelection().toString();
-//   if (text) {
-//     selection.current = text;
-//   }
-// };
-
-// useEffect(() => {
-//   document.addEventListener("selectionchange", handleSelection);
-//   return () => {
-//     document.removeEventListener("selectionchange", handleSelection);
-//   };
-// });
-
-// export default GetSelectionHandler;
 import React, { useState } from "react";
 import GetDefinition from "./getDefinition";
-// import DefinitionPopUp from "./getDefinition";
-// let popUpDiv;
+
 let selectedDiv;
 let selectedDivObject;
 
